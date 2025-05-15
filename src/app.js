f137dbbcea95c9604235af3ef7b8b6936de66ff3
Andressa Monteiro <dekomonte@yahoo.com.br>
2025-05-15 11:17:44 -0300

    Adicionando criação de produto

--- src/app.js
+++ src/app.js
@@ -10,6 +10,9 @@ const router = express.Router();
 //Conexao com o banco de dados
 mongoose.connect('mongodb://root:Flamengo_2019$%40@localhost:27017/admin');
 
+//Carrega os Models
+const Product = require('./models/product');
+
 //Carrega as rotas
 const indexRoutes = require('./routes/index-route');
 const productRoute = require('./routes/product-route');
