b069312dab8b22e6806d2e841b31ab46b070f622
Andressa Monteiro <dekomonte@yahoo.com.br>
2025-09-19 22:18:09 -0300

    Igualando projeto com repositório do curso

--- src/app.js
+++ src/app.js
@@ -3,26 +3,43 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
+const config = require('./config');
 
 const app = express();
 const router = express.Router();
 
-//Conexao com o banco de dados
+// Connecta ao banco
 mongoose.connect('mongodb://root:Flamengo_2019$%40@localhost:27017/admin');
 
-//Carrega os Models
+// Carrega os Models
 const Product = require('./models/product');
+const Customer = require('./models/customer');
+const Order = require('./models/order');
 
-//Carrega as rotas
-const indexRoutes = require('./routes/index-route');
+// Carrega as Rotas
+const indexRoute = require('./routes/index-route');
 const productRoute = require('./routes/product-route');
+const customerRoute = require('./routes/customer-route');
+const orderRoute = require('./routes/order-route');
 
-app.use(bodyParser.json());
+app.use(bodyParser.json({
+    limit: '5mb'
+}));
 app.use(bodyParser.urlencoded({
     extended: false
 }));
 
-app.use('/', indexRoutes);
-app.use('/products', productRoute);
+// Habilita o CORS
+app.use(function (req, res, next) {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, x-access-token');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+    next();
+});
 
-module.exports = app; 
+app.use('/', indexRoute);
+app.use('/products', productRoute);
+app.use('/customers', customerRoute);
+app.use('/orders', orderRoute);
+
+module.exports = app;
